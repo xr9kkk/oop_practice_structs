7fac860b8877197cd266c48c31223d71e59ffb80
Your Name <you@example.com>
2024-11-14 23:16:08 +0300

    already done

--- oop_practice_structs/Character.h
+++ oop_practice_structs/Character.h
@@ -1,24 +1,36 @@
 #pragma once
 #include <string>
-#include"Coordinates.h"
+#include "Coordinates.h"
 #include <vector>
 
-
-class Character
-{
+class Character {
 private:
-	std::string name;
-	Coordinates* coordinates;
-	int level{}, hp{};
-	const int spawn_radius{ 20 };
-public:
-	Character(std::string name, Coordinates* c, int level, int hp);
-	~Character();
-	std::string get_name() { return name; };
-	Coordinates* get_coordinates() { return coordinates;};
-	int get_level() { return level; };
-	int get_hp() { return hp;};
-	Character* spawn_copy();
-	Character* attack(std::vector<Character*> l);
-};
+    std::string name;
+    Coordinates* coordinates;
+    int level{};
+    int hp{};
+    const int spawn_radius{ 20 };
+    int turns_alive{ 0 };
+    Character* parent{ nullptr }; 
 
+public:
+    Character(std::string name, Coordinates* c, int level, int hp, Character* parent = nullptr)
+        : name(name), coordinates(c), level(level), hp(hp), parent(parent) {}
+
+    ~Character() { delete coordinates; }
+
+    std::string get_name() const { return name; }
+    Coordinates* get_coordinates() const { return coordinates; }
+    int get_level() const { return level; }
+    int get_hp() const { return hp; }
+    void decrease_hp(int amount) { hp -= amount; }
+    void increment_turns() { ++turns_alive; }
+    int get_turns_alive() const { return turns_alive; }
+
+    bool is_copy() const { return parent != nullptr; }
+    bool is_dead() const { return hp <= 0; }
+
+    Character* spawn_copy(); 
+    Character* attack(std::vector<Character*>& list);
+    Character* get_parent() const { return parent; } 
+};
--- oop_practice_structs/FileName.cpp
+++ oop_practice_structs/FileName.cpp
@@ -1,55 +1,91 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
 #include "Character.h"
 #include "DrawController.h"
-#include <thread>
 
-int main()
-{
-	using namespace std::chrono_literals;
-	srand(time(NULL));
-	std::vector<Character*>l;
-	l.push_back(new Character("1", new Coordinates(0, 0), 10, 20));
-	l.push_back(new Character("2", new Coordinates(10, 0), 5, 15));
-	l.push_back(new Character("3", new Coordinates(50, 20), 1, 30));
-	l.push_back(new Character("4", new Coordinates(100, -20), 20, 20));
+int main() {
+    using namespace std::chrono_literals;
+    srand(time(NULL));
 
-	DrawController* dc = new DrawController();
-	for (Character* c : l) {
-		dc->draw_character(c);
-	}
-	for (int i{}; i<10; ++i)
-	{
-		std::vector<Character*> copies;
-		for (int i{ 0 }; i < l.size(); ++i)
-		{
-			if (rand() % 100 < 25)
-			{
-				Character* ccopy = l[i]->spawn_copy();
-				copies.push_back(ccopy);
-				std::cout << l[i]->get_name() << " Creates copy " << ccopy->get_name() << std::endl;
-			}
-			else if (rand() % 100 < 50)
-			{
-				Character* target = l[i]->attack(l);
-				std::cout << l[i]->get_name() << " attacks " << target->get_name() << std::endl;
-			}
-		}
-		for (int i{ 0 }; i < l.size(); ++i)
-		{
-			if (l[i]->get_hp() <= 0)
-			{
-				std::cout << l[i]->get_name() << " Has died..." << std::endl;
-			}
-			else
-			{
-				copies.push_back(l[i]);
-			}
-		}
-		l = copies;
-		for (Character* c : l) {
-			dc->draw_character(c);
-		}
-	}
+    std::vector<Character*> list;
+    list.push_back(new Character("Sasha", new Coordinates(0, 0), 10, 5));
+    list.push_back(new Character("Alexandr", new Coordinates(10, 0), 20, 10));
+    list.push_back(new Character("Sanya", new Coordinates(50, 20), 10, 5));
+    list.push_back(new Character("Olesya", new Coordinates(100, -20), 5, 20));
+    list.push_back(new Character("Kirill", new Coordinates(100, -20), 20, 9));
 
+    DrawController* dc = new DrawController();
+
+    for (Character* c : list) {
+        dc->draw_character(c);
+    }
+
+    while (true) {
+        std::vector<Character*> next_round;
+        int alive_count = 0;
+        for (int i = 0; i < list.size(); ++i) {
+            list[i]->increment_turns();
+            if (list[i]->get_hp() > 0) {
+                alive_count++;
+            }
+
+            if (rand() % 100 < 25) {
+                Character* copy = list[i]->spawn_copy();
+                next_round.push_back(copy);
+                std::cout << list[i]->get_name() << " creates copy " << copy->get_name() << std::endl;
+            }
+            else if (rand() % 100 < 50) {
+                Character* target = list[i]->attack(list);
+                std::cout << list[i]->get_name() << " attacks " << target->get_name() << std::endl;
+            }
+        }
+
+        for (int i = 0; i < list.size(); ++i) {
+            if (list[i]->is_dead()) {
+                std::cout << list[i]->get_name() << " has died..." << std::endl;
+            }
+            else {
+                if (list[i]->is_copy() && list[i]->get_turns_alive() > 2) {
+                    std::cout << list[i]->get_name() << " copy has lived for 2 turns and dies..." << std::endl;
+                }
+                else {
+                    next_round.push_back(list[i]);
+                }
+            }
+        }
+
+        list = next_round;
+
+        for (Character* c : list) {
+            dc->draw_character(c);
+        }
+
+        if (alive_count <= 1) {
+            std::cout << "Only one character is left alive!" << std::endl;
+
+            // Вывод информации о выжившем персонаже
+            for (Character* c : list) {
+                if (!c->is_dead()) {
+                    std::cout << "The winner is: " << c->get_name() << std::endl;
+                    std::cout << "Level: " << c->get_level() << ", HP: " << c->get_hp() << std::endl;
+                    std::cout << "Coordinates: (" << c->get_coordinates()->get_x() << ", " << c->get_coordinates()->get_y() << ")\n";
+                }
+            }
+
+            break;
+        }
+
+        std::this_thread::sleep_for(100ms);
+    }
+
+    // Очистка памяти
+    for (Character* c : list) {
+        delete c;
+    }
+    delete dc;
+
+    return 0;
 }
